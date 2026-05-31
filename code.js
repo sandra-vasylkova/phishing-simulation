1f042aa24ce65ad86a41c26b5513dd94e61a2484
Sandra Vasylkova <sandravasylkova@gmail.com>
2026-05-31 19:20:33 +0200

    fixes 4.0

--- code.js
+++ code.js
@@ -4,19 +4,19 @@ form.addEventListener("submit", async function (event) {
   event.preventDefault();
 
   const username = document.getElementById("username").value.trim();
-  const course = document.getElementById("password").value.trim();
+  const password = document.getElementById("password").value.trim();
 
   if (username.length < 1) {
     alert("Geben Sie Ihren Benutzernamen ein.");
     return;
   }
 
-  if (course.length < 1) {
+  if (password.length < 1) {
     alert("Geben Sie Ihr Passwort ein.");
     return;
   }
 
-  const response = await fetch("/api/save-player", {
+  const response = await fetch("/api/savedData", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
